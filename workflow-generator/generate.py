f03ebf3e53240d11b8a117ef3e68a08504431f49
Nik Karavias <nikos@koutaki.local>
2023-05-24 19:48:48 -0400

    Introduce a workflow mock cli option that generates a workflow where all deployments are set inactive

--- workflow-generator/generate.py
+++ workflow-generator/generate.py
@@ -26,9 +26,21 @@ def create_workflow(args):
     with open(args.output_file, 'w') as f:
         f.write(workflow.to_yaml())
 
+
+def mock_workflow(args):
+    print(args)
+    workflow_template_path = "../workflow_template_test.yaml"
+    workflow_manager = WorkflowManager(workflow_template_path, [])
+    workflow = workflow_manager.mock_workflow()
+
+    with open(args.output_file, 'w') as f:
+        f.write(workflow.to_yaml())
+
+
 def validate_trigger(args):
     # Perform trigger validation
     pass
+
 
 def main():
     parser = create_parser()
@@ -37,6 +49,8 @@ def main():
     if args.command == 'workflow':
         if args.workflow_command == 'create':
             create_workflow(args)
+        elif args.workflow_command == 'mock':
+            mock_workflow(args)
         else:
             print("Invalid workflow command.")
     elif args.command == 'trigger':
